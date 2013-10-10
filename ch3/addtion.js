37fb3d923051caf094cedde659c4e4bfaa629402
nie xin <niexin.study@gmail.com>
2013-10-10 22:45:47 +0200

    ch3 p107

---
+++ ch3/addtion.js
@@ -0,0 +1,23 @@
+var num1 = 5;
+var num2 = 10;
+var message = "The sum of 5 and 10 is " + (num1 + num2);
+console.log(message);
+
+var result1 = 5 + 10;
+console.log(result1);
+var result2 = 5 + "10";
+console.log(result2);
+
+var result3 = 5 - true;
+var result4 = NaN - 1;
+var result5 = 5 - 3;
+var result6 = 5 - "";
+var result7 = 5 - "2";
+var result8 = 5 - null;
+console.log(result3);
+console.log(result4);
+console.log(result5);
+console.log(result6);
+console.log(result7);
+console.log(result8);
+
