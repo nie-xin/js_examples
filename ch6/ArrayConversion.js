411ee4087e9bae8f42ffc5aabb2270e18856f9f9
nie xin <niexin.study@gmail.com>
2013-10-29 13:02:17 +0100

    js go

---
+++ ch6/ArrayConversion.js
@@ -0,0 +1,37 @@
+// // Array conversion examples
+var colors = ["red", "blue", "green"];
+
+// recontruct string wit the defined separator
+console.log(colors.join(","));
+console.log(colors.join("||"));
+
+// // array to string
+// console.log(colors.toString());
+// console.log(colors.valueOf());
+// console.log(colors);	// colors will be called to toString coz console.log need a string as parameter
+
+// var person1 = {
+// 	toLocaleString : function () {
+// 		return "Nikolaos";
+// 	},
+
+// 	toString : function () {
+// 		return "Nicolas";
+// 	}
+// };
+
+// var person2 = {
+// 	toLocaleString : function () {
+// 		return "NieXin";
+// 	},
+
+// 	toString : function () {
+// 		return "Xinxin";
+// 	}
+// };
+
+// var people = [person1, person2];
+// console.log(people);
+// console.log(people.toString());
+// console.log(people.toLocaleString());
+
