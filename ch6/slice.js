03bc03040528c449f67873f4685ccb19172bbc83
nie xin <niexin.study@gmail.com>
2013-11-07 00:21:00 +0100

    go go

---
+++ ch6/slice.js
@@ -0,0 +1,7 @@
+var colors = ["red", "green", "blue", "yellow", "purple"];
+var colors2 = colors.slice(1);
+// 1 ~ 4, but not include the 4
+var colors3 = colors.slice(1, 4);
+
+console.log(colors2);
+console.log(colors3);
