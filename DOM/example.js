de05205357329e3efeef3f34e94fc67853d5e78a
nie xin <niexin.study@gmail.com>
2013-12-18 23:05:00 +0100

    ch7 done

--- DOM/example.js
+++ DOM/example.js
@@ -1,14 +1,27 @@
+// window.onload = function() {
+// 	// var testdiv = document.getElementById("testdiv");
+// 	// testdiv.innerHTML = "<p>I inserted <em>this</em> content.</p>";
+// 	var para = document.createElement("p");
+// 	var txt = document.createTextNode("Hello world");
+// 	para.appendChild(txt);
+// 	var testdiv = document.getElementById("testdiv");
+// 	testdiv.appendChild(para);
+// 	// var info = "nodeName: ";
+// 	// info += para.nodeName;
+// 	// info += " nodeType: ";
+// 	// info += para.nodeType;
+// 	// alert(info);
+// }
 window.onload = function() {
-	// var testdiv = document.getElementById("testdiv");
-	// testdiv.innerHTML = "<p>I inserted <em>this</em> content.</p>";
-	var para = document.createElement("p");
-	var txt = document.createTextNode("Hello world");
-	para.appendChild(txt);
-	var testdiv = document.getElementById("testdiv");
-	testdiv.appendChild(para);
-	// var info = "nodeName: ";
-	// info += para.nodeName;
-	// info += " nodeType: ";
-	// info += para.nodeType;
-	// alert(info);
+    var para = document.createElement("p");
+    var txt1 = document.createTextNode("This is ");
+    para.appendChild(txt1);
+    var emphasis = document.createElement("em");
+    var txt2 = document.createTextNode("my");
+    emphasis.appendChild(txt2);
+    para.appendChild(emphasis);
+    var txt3 = document.createTextNode(" content.");
+    para.appendChild(txt3);
+    var testdiv = document.getElementById("testdiv");
+    testdiv.appendChild(para);
 }
